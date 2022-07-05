202f9f9b8e019ef6173659aca349a21986093fa9
Muhammad Ilham <108717436+CaamMd@users.noreply.github.com>
2022-07-06 02:51:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 let fs = require('fs')                    //CREATOR = GEMPY , DEVBOT
 let chalk = require('chalk')
 global.owner = [
-  ['6287898307350'],
-  ['66287898307350'],
-  ['6287898307350', 'gempy', true]  
+  ['6289653007846'],
+  ['6289653007846'],
+  ['6289653007846', 'Caam', true]  
   
 ] 
-global.ownername = [['Gempy']]
-global.namabot = [['PENTABOT']]
-global.ownerinsta = [['@gmpyyy']]
+global.ownername = [['Caam']]
+global.namabot = [['KiritoBotz']]
+global.ownerinsta = [['@cccaamm_']]
 global.mods = [] 
 global.prems = [] 
 
@@ -43,8 +43,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'PENTABOT'
-  var sticker_author = 'By GEMPY'
+  var sticker_name = 'KiritoBotz'
+  var sticker_author = 'By Caam'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -61,7 +61,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = 'Created by Gempy'
+global.wm = 'Created by Caam'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
